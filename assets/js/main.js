0d6cb6986f67c62a64a99abb1021157e68ed08a8
timokonkwo <chibuiketimokonkwo@gmail.com>
2022-03-23 14:42:17 +0100

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle')
+navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU ON LINK CLICK ===============*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkClicked() {
+    navMenu.classList.remove('show-menu');
+}
+// Remove the Nav Menu when a link is clicked.
+navLink.forEach(link => link.addEventListener('click', linkClicked))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== QUESTIONS ACCORDION ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/
+
+// Previously selected topic (if user selected)
+
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+
+// We validate if the user previously chose a topic
+
+
+// Activate / deactivate the theme manually with the button
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
