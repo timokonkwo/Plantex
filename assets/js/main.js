0e209a39bd2e19a0a8eb00e21336ac67b2eac002
timokonkwo <chibuiketimokonkwo@gmail.com>
2022-07-21 18:55:03 +0100

    updating

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,9 +72,31 @@ const toggleItem = (item) => {
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+// sections.forEach(item => {
+//     console.log(item.offsetHeight, item.offsetTop)
+// })
 
 
-/*=============== SHOW SCROLL UP ===============*/
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    })
+}
+
+
+window.addEventListener('scroll', scrollActive)
+    /*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== DARK LIGHT THEME ===============*/
